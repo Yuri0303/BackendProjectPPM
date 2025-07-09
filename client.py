b6c49499d6e244bd025fb73088957889e4a05fcd
Yuri <yuri.bartoletti@gmail.com>
2025-07-10 00:48:04 +0200

    Refactor project

--- client.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-BASE = 'http://localhost:5000/'
-
-request = requests.post(BASE + '/auth/login', {'username': 'pippo', 'password': 'poppo'})
-response = request.json()
-print(response)
--- db.py
+++
@@ -1,6 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-def to_dict(obj):
-    return {c.name: getattr(obj, c.name) for c in obj.__table__.columns}
